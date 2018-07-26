11b07ebd9f81faaa97577cf24dcdfafdc49241c9
Martin Boss <martin.klissarov@polymtl.ca>
2018-07-26 15:01:07 -0700

    fixes in build

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='my_gym',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym==0.9.3','mujoco-py==0.5.7']  # And any other dependencies foo needs
 )  
