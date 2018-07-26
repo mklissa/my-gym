be987a71329ed7ee1d38354d44b65822d22aad38
Martin Boss <martin.klissarov@polymtl.ca>
2018-07-26 14:07:53 -0700

    added cheetah

--- my_gym/__init__.py
+++ my_gym/__init__.py
@@ -5,3 +5,10 @@ register(
     entry_point='my_gym.envs.sparsemountaincar:SparseMountainCarEnv',
     max_episode_steps=400,
 )
+
+register(
+    id='SparseHalfCheetah-v0',
+    entry_point='my_gym.envs.sparsehalf_cheetah:SparseHalfCheetahEnv',
+    max_episode_steps=1000,
+    reward_threshold=4800.0,
+)
--- my_gym/envs/__init__.py
+++ my_gym/envs/__init__.py
@@ -1,1 +1,4 @@
+from my_gym.envs.mujoco_env import MujocoEnv
+# from gym.envs.mujoco.mujoco_env import MujocoEnv
 from my_gym.envs.sparsemountaincar import SparseMountainCarEnv
+from my_gym.envs.sparsehalf_cheetah import SparseHalfCheetahEnv
